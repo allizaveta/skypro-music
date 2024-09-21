d3e96253aa45b2e6ce1b25ca1b9ac270ed1995c9
Allizaveta <allizaveta@gmail.com>
2024-09-21 20:43:43 +0300

    НУЖЕ НУ ДАВАЙ ЖЕ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
-import Sidebar from "./components/Sidebar/Sidebar";
-import Bar from "./components/Bar/Bar";
-import { Centerblock } from "./components/Centerblock/Centerblock";
-import Nav from "./components/Nav/Nav";
+"use client";
 import { CurrentTrackProvider } from "@/contexts/CurrentTrackProvider";
 
+import { useAppSelector } from "@/store/store";
+import { useFilteredTracks } from "@/hooks/useFilteredTracks";
+import { Centerblock } from "./components/Centerblock/Centerblock";
+import Nav from "./components/Nav/Nav";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Bar from "./components/Bar/Bar";
+
 export default function Home() {
+  const { allTracks, error } = useAppSelector((state) => state.playlist);
+  const filteredTracks = useFilteredTracks(allTracks);
 
   return (
     <div className="wrapper">
       <div className="container">
-      <CurrentTrackProvider>
-        <main className="main">
-          <Nav />
-          <Centerblock />
-          <Sidebar />
-        </main>
-        <Bar />
+        <CurrentTrackProvider>
+          <main className="main">
+            <Nav />
+            <Centerblock tracks={filteredTracks} title={"Все треки"} />
+            <Sidebar />
+          </main>
+          <Bar />
         </CurrentTrackProvider>
         <footer className="footer" />
       </div>
