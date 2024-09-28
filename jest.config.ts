fd2b5b5e5bbfa2fe7fe442e95eaa1619764d4314
Allizaveta <allizaveta@gmail.com>
2024-09-28 08:25:48 +0300

    зывыаывацу кбпигнор

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,11 @@
 import type { Config } from "jest";
-import nextJest from "next/jest.js";
-
-const createJestConfig = nextJest({
-  dir: "./",
-});
 
 const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-
-  moduleNameMapper: {
-    "^@/components/(.*)$": "<rootDir>/components/$1",
-  },
 };
 
-export default createJestConfig(config);
+export default config;
